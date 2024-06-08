2b84d80bf56223e6465a598e6d7bc37c642f6681
LuisPadillaAR <luis0907p@gmail.com>
2024-06-07 22:58:28 -0500

    edit

--- app/src/components/login/Login.jsx
+++ app/src/components/login/Login.jsx
@@ -4,10 +4,6 @@ import './loginStyle.css'
 
 export const Login = () => {
 
-    const login = () => {
-        document.location.href = '/';
-    }
-
     return (
         <div className='container'>
             <div className="form__container">
@@ -15,7 +11,6 @@ export const Login = () => {
                 <input className="form__input form__input-user" type="text" name="user" id="user" placeholder="User" />
                 <input className="form__input form__input-password" type="password" name="password" id="password" placeholder="Password" />
                 <Link className='form__btn form__btn-login' to={'/'}>Login</Link>
-                {/* <input className="form__btn form__btn-login" type="button" value="Login" onClick={login} /> */}
                 <p className='form__links'>¿No tienes una cuentas? Entra
                     <Link to={'/register'}> aquí</Link>
                 </p>
