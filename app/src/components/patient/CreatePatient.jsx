608564bd40e4f89542cad62e4b918c293b129c16
LuisPadillaAR <luis0907p@gmail.com>
2024-06-27 22:41:00 -0500

    mm

--- app/src/components/patient/CreatePatient.jsx
+++ app/src/components/patient/CreatePatient.jsx
@@ -1,5 +1,5 @@
 
-import { Grid, Link } from "@mui/material";
+import { Grid, Link, TextField, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import NavBar from "../navbar/NavBar";
 
@@ -11,7 +11,28 @@ export const CreatePatient = () => {
             </Grid>
             <Grid item md={12}>
                 <Grid container columns={12} sx={{p: 3}}> 
-                    
+                    <Grid item md={12}>
+                        <Typography variant="h3">Nuevo</Typography>
+                    </Grid>
+                    <Grid item padding={'10px'}>
+                        <Grid container flexDirection={'row'} gap={2}>
+                            <Grid item>
+                                <TextField variant="outlined" label='Nombre'/>
+                            </Grid>
+                            <Grid item>
+                                <TextField variant="outlined" label='Tipo'/>
+                            </Grid>
+                            <Grid item>
+                                <TextField variant="outlined" label='tamano'/>
+                            </Grid>
+                            <Grid item>
+                                <TextField variant="outlined" label='peso'/>
+                            </Grid>
+                            <Grid item>
+                                <TextField variant="outlined" label='Voluntario'/>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </Grid>
