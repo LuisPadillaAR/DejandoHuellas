9d1209ee176ce36f297e93b1b8d999dccd9416f5
LuisPadillaAR <luis0907p@gmail.com>
2024-06-29 00:19:33 -0500

    mm

--- API/index.js
+++ API/index.js
@@ -1,11 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import db from './db.js'
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT
 
-app.listen(port, () => {
-    console.log('Conectado');
+
+
+db.then(() => {
+    console.log('base de datos conectada')
+    app.listen(port, () => {
+        console.log('Conectado');
+    })
+}).catch((err) => console.log(err))
+
+app.get('/', (req, res) => {
+    res.json({name: 'Luis Padilla', age: 23})
 })
