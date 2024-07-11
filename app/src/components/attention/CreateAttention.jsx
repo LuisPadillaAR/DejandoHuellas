ce9ea6846cbd9532f7a3ae5071183ab49e124f1e
Luis Padilla <luis0907p@gmail.com>
2024-07-10 22:09:24 -0400

    dd

---
+++ app/src/components/attention/CreateAttention.jsx
@@ -0,0 +1,89 @@
+
+import { Grid, TextField, Typography, Link} from "@mui/material";
+import { Link as RouterLink } from 'react-router-dom';
+import NavBar from "../navbar/NavBar";
+import axios from 'axios'
+import { useEffect, useState } from "react";
+
+export const CreatePatient = () => {
+
+
+    const [name, setName] = useState("");
+    const [species, setSpecies] = useState("")
+    const [breed,  setBreed] = useState("")
+    const [color, setColor] = useState("")
+    const [rescue_date, setRescue_date] = useState("")
+    const [size, setSize] = useState("")
+    const [weight, setWeight] = useState("")
+    const [age, setAge] = useState("")
+
+
+    const createPatient = async () => {
+        await axios.post(`http://localhost:3030/patients/create`, 
+            {
+                name: name,
+                species: species,
+                breed:breed,
+                color: color,
+                rescue_date: rescue_date,
+                size: size,
+                weight: weight,
+                age: age
+             })
+        .then(() =>  alert('creado'))
+        .catch(() => console.log('error'))
+    }
+
+    return (
+        <Grid container columns={12}>
+            <Grid item md={12}>
+                <NavBar />
+            </Grid>
+            <Grid item md={12}>
+                <Grid container columns={12} sx={{p: 3}}> 
+                    <Grid item md={12}>
+                        <Typography variant="h4">Nuevo Paciente</Typography>
+                    </Grid>
+                    <Grid item padding={'10px'}>
+                        <Grid container columns={12} flexDirection={'row'} gap={2}>
+                            <Grid item md={4}>
+                                <TextField variant="outlined" label='Nombre' onChange={(e) => setName(e.target.value)} />
+                            </Grid>
+                            <Grid item md={4}>
+                                <TextField variant="outlined" label="Especie" onChange={(e) => setSpecies(e.target.value)} />
+                            </Grid>
+                            <Grid item md={4}>
+                                <TextField variant="outlined" label='Raza' onChange={(e) => setBreed(e.target.value)} />
+                            </Grid>
+                            <Grid item md={4}>
+                                <TextField variant="outlined"  label='Color'onChange={(e) => setColor(e.target.value)} />
+                            </Grid>
+                            <Grid item md={4}>
+                                <TextField variant="outlined" label='Fecha de rescate'onChange={(e) => setRescue_date(e.target.value)} />
+                            </Grid>
+                            <Grid item md={4}>
+                                <TextField variant="outlined" label='Tamaño' onChange={(e) => setSize(e.target.value)} />
+                            </Grid>
+                            <Grid item md={4}>
+                                <TextField variant="outlined" label='peso' onChange={(e) => setWeight(e.target.value)} />
+                            </Grid>
+                            <Grid item md={12}>
+                                <TextField variant="outlined" label='Edad' onChange={(e) => setAge(e.target.value)} />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Grid container gap={1}>
+                            <Grid item>
+                                <Link variant="button" onClick={createPatient} to={'/patients'} component={RouterLink} >Guardar</Link>
+                            </Grid>
+                            <Grid item>
+                                <Link variant="button" component={RouterLink} to={"/patients"} >Cancelar</Link>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
--- app/src/components/statistics/Statistics.jsx
+++ app/src/components/statistics/Statistics.jsx
@@ -1,7 +1,24 @@
 import { Grid } from "@mui/material";
 import NavBar from "../navbar/NavBar";
+import axios from "axios"
+import { useEffect, useState } from "react";
 
 export default function Statistics() {
+
+
+  const [datos, setDatos] = useState([]);
+
+  useEffect(() => {
+      (() => {
+        axios.get('http://localhost:3030/patients/')
+        .then((dat) => {
+          setDatos(dat.data)
+        })
+      })()
+  },[])
+
+
+
   return (
     <Grid container columns={12}>
       <Grid item md={12}>
@@ -9,6 +26,18 @@ export default function Statistics() {
       </Grid>
       <Grid item md={12}>
             <p>Estadisticas</p>
+            <p>{datos?.map(e => {
+              const att = e.attentions
+              return (
+                att?.map((t) => {
+                console.log(t.operation);
+                return (
+                  <p>{t.operation}</p>
+                )
+              })
+              )
+              
+            })}</p>
       </Grid>
     </Grid>
   );
